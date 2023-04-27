11171d2400759fa7aee486b2f810918130281bbb
yurui01 <489593544@qq.com>
2023-04-27 17:54:59 +0800

    01: Text

---
+++ src/app/01/page.tsx
@@ -0,0 +1,20 @@
+import "./styles.css";
+
+export default function CSS01() {
+  return (
+    <div className="flex min-h-screen">
+      <div className="frame">
+        <div className="center">
+          <div className="number">
+            <span className="one-one" />
+            <span className="one-two" />
+            <span className="zero-one" />
+            <span className="zero-two" />
+          </div>
+          <div className="big-text">Days</div>
+          <div className="small-text">CSS challenge</div>
+        </div>
+      </div>
+    </div>
+  );
+}

b7ca01be305dc48b29be68754f233fc9191c2729
余瑞 <pazzitul@163.com>
2023-05-04 14:14:29 +0800

    03: animation logo, update home page

---
+++ src/app/03/page.tsx
@@ -0,0 +1,21 @@
+"use client";
+import "./styles.css";
+
+export default function CSS03() {
+  return (
+    <div className="flex min-h-screen items-center justify-center">
+      <div className="frame">
+        <div className="center">
+          <div className="circle">
+            <div className="sky"></div>
+            <div className="ground"></div>
+            <div className="sun"></div>
+            <div className="side-left"></div>
+            <div className="side-right"></div>
+            <div className="shadow"></div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
